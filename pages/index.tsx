ad13cb8e09d2b68e45750afd29313b1d94770170
sayoi341 <20alt341@gmail.com>
2021-11-21 00:16:33 +0900

    test: husky

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,7 +14,7 @@ const Home: NextPage = () => {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          ようこそ <a href="https://nextjs.org">Next.js</a> へ!
         </h1>
 
         <p className={styles.description}>
