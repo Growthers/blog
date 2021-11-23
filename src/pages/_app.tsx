96fb027a0ad37dd8a7c65057ea42d18ad8445b96
sayoi341 <20alt341@gmail.com>
2021-11-23 16:35:13 +0900

    fix: storybookの設定を変更

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,11 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
 
 const MyApp = ({ Component, pageProps }: AppProps): JSX.Element => (
   <>
     {/* eslint-disable-next-line react/jsx-props-no-spreading */}
     <Component {...pageProps} />
   </>
-)
+);
 
-export default MyApp
+export default MyApp;
