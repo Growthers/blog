4807825ec75d01b1746907710a87e53cd158e73a
Kota Kato <peony.btn@gmail.com>
2021-11-26 00:07:11 +0900

    Fix ESLint

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,35 +13,34 @@ export const getStaticProps = () => {
   }
 }
 
-const Home: NextPage<Props> = (props) => {
-  return (
+const Home: NextPage<Props> = (props) => (
+  <div>
     <div>
-       <div>
-         記事一覧
-       </div>
-       <div>
-         <ul>
-           {props.posts.map((post) => (
-             <li key={post.title}>
-               {post.title}
-               <ul>
-                 <li>
-                   <Link href={`/authors/${post.author}/posts/${post.slug}`}>
-                     {post.author + "の" + post.slug}
-                   </Link>
-                 </li>
-                 <li>
-                   <Link href={`/authors/${post.author}`}>
-                     {post.author + "のページ"}
-                   </Link>
-                 </li>
-               </ul>
-             </li>
-           ))}
-         </ul>
-       </div>
-     </div>
-   )
- }
+      記事一覧
+    </div>
+    <div>
+      <ul>
+        {props.posts.map((post) => (
+          <li key={post.title}>
+            {post.title}
+            <ul>
+              <li>
+                <Link href={`/authors/${post.author}/posts/${post.slug}`}>
+                  {post.author + "の" + post.slug}
+                </Link>
+              </li>
+              <li>
+                <Link href={`/authors/${post.author}`}>
+                  {post.author + "のページ"}
+                </Link>
+              </li>
+            </ul>
+          </li>
+        ))}
+      </ul>
+    </div>
+  </div>
+)
+
 
 export default Home
