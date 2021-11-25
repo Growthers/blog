688bafc3aef59c8446995eda1ffd626c0d464500
Kota Kato <peony.btn@gmail.com>
2021-11-25 23:53:59 +0900

    Fix: src/pages/index.tsx
    トップページ記事一覧サンプルがぶっ壊れていたので修正

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,47 @@
-import type { NextPage } from "next";
+import { InferGetStaticPropsType, NextPage } from 'next'
+import Link from 'next/link'
+import { getPosts } from '../utils/api'
 
-const index: NextPage = () => (
-  <div>
-    <h2>Hello Next.js</h2>
-  </div>
-);
+type Props = InferGetStaticPropsType<typeof getStaticProps>;
 
-export default index;
+export const getStaticProps = () => {
+  const posts = getPosts()
+  return {
+    props: {
+      posts
+    }
+  }
+}
+
+const Home: NextPage<Props> = (props) => {
+  return (
+    <div>
+       <div>
+         記事一覧
+       </div>
+       <div>
+         <ul>
+           {props.posts.map((post) => (
+             <li key={post.title}>
+               {post.title}
+               <ul>
+                 <li>
+                   <Link href={`/authors/${post.author}/posts/${post.slug}`}>
+                     {post.author + "の" + post.slug}
+                   </Link>
+                 </li>
+                 <li>
+                   <Link href={`/authors/${post.author}`}>
+                     {post.author + "のページ"}
+                   </Link>
+                 </li>
+               </ul>
+             </li>
+           ))}
+         </ul>
+       </div>
+     </div>
+   )
+ }
+
+export default Home
