36e981f0eb293f22fbc08c985049dfaf4330cb89
Yusuke4869 <67366917+Yusuke4869@users.noreply.github.com>
2021-11-29 19:46:31 +0900

    update: style

--- src/components/Author.tsx
+++ src/components/Author.tsx
@@ -21,14 +21,14 @@ const Author: FC<Props> = (props) => {
   const isTwitterID = TwitterID !== "";
 
   return (
-    <div className="bg-white py-8 flex items-start">
+    <div className="bg-white py-8 flex items-start w-full">
       {/* eslint-disable-next-line @next/next/no-img-element */}
       {isIconURL && <img className="w-16 h-16 rounded-full" src={IconURL} alt={AuthorName} />}
       {!isIconURL && <FaUserCircle className="w-16 h-16" />}
       <div className="ml-4">
         <div className="flex items-center">
           <p className="text-lg font-black">{AuthorName}</p>
-          <div className="mx-2 text-sm flex items-end">
+          <div className="mx-2 text-sm flex items-end flex-wrap">
             {Roles.map((value) => (
               <p className="mx-1 p-1 bg-gray-300 rounded-md" key={value}>
                 {value}
@@ -36,7 +36,7 @@ const Author: FC<Props> = (props) => {
             ))}
           </div>
         </div>
-        <div className="my-3">{Bio}</div>
+        <div className="my-3 break-all">{Bio}</div>
         <div className="flex items-center">
           {isSiteURL && (
             <div className="mr-4 flex items-center">
