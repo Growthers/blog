2df5998feb9790e4d9849e978f24924a2c608544
sayoi341 <20alt341@gmail.com>
2021-12-01 00:28:07 +0900

    fix/表示制限

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,24 +7,26 @@ import Layout from "components/Layout";
 type Props = InferGetStaticPropsType<typeof getStaticProps>;
 
 export const getStaticProps = () => {
-  const posts = getPosts().sort((a, b) => {
-    if (a.date === "") return 1;
-    if (b.date === "") return -1;
+  const posts = getPosts()
+    .sort((a, b) => {
+      if (a.date === "") return 1;
+      if (b.date === "") return -1;
 
-    const aD = new Date(a.date);
-    const bD = new Date(b.date);
+      const aD = new Date(a.date);
+      const bD = new Date(b.date);
 
-    if (aD.getFullYear() === bD.getFullYear()) {
-      if (aD.getMonth() === bD.getMonth()) {
-        if (aD.getDate() === bD.getDate()) {
-          return 0;
+      if (aD.getFullYear() === bD.getFullYear()) {
+        if (aD.getMonth() === bD.getMonth()) {
+          if (aD.getDate() === bD.getDate()) {
+            return 0;
+          }
+          return bD.getDate() - aD.getDate();
         }
-        return bD.getDate() - aD.getDate();
+        return bD.getMonth() - aD.getMonth();
       }
-      return bD.getMonth() - aD.getMonth();
-    }
-    return bD.getFullYear() - aD.getFullYear();
-  });
+      return bD.getFullYear() - aD.getFullYear();
+    })
+    .slice(0, 21);
 
   return {
     props: {
