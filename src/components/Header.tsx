5ee0677d4b0c803f94c1664ec3273ca51c9de523
Yusuke4869 <67366917+Yusuke4869@users.noreply.github.com>
2021-12-01 07:41:55 +0900

    add: responsive header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,20 +1,17 @@
 import type { FC } from "react";
-import Image from "next/image";
 import Link from "next/link";
 
-import logo from "../../public/growthers.png";
-
 const Header: FC = () => (
-  <div className="bg-white p-4 px-20 flex items-center">
+  <div className="bg-white p-3 sm:p-6 flex items-center">
     <div>
       <Link href="/">
         <a className="flex justify-center items-center">
-          <Image className="rounded-full h-12 w-12" src={logo} alt="growthers" width={45} height={45} />
-          <p className="ml-2 text-lg">共同開発鯖 - Blog</p>
+          <img className="rounded-full h-6 w-6 sm:h-8 sm:w-8 md:h-10 md:w-10" src="/growthers.png" alt="growthers" />
+          <p className="ml-1 md:ml-2 text-sm sm:text-lg md:text-xl">共同開発鯖 - Blog</p>
         </a>
       </Link>
     </div>
-    <div className="px-20">
+    <div className="px-4 sm:px-8 lg:px-12 text-sm sm:text-base md:text-lg">
       <Link href="/">
         <a>Top</a>
       </Link>
