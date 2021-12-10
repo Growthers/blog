f367410653723a94c663fcf0000b87e258bf4fb4
Yusuke4869 <67366917+Yusuke4869@users.noreply.github.com>
2021-12-10 11:31:55 +0900

    fix: redirect to author's page

--- src/pages/[author]/posts/index.tsx
+++ src/pages/[author]/posts/index.tsx
@@ -1,6 +1,30 @@
-import { NextPage } from "next";
+import { GetStaticPaths, GetStaticProps, NextPage } from "next";
 import { useEffect } from "react";
 import { useRouter } from "next/router";
+import { ParsedUrlQuery } from "node:querystring";
+
+import { getPosts } from "utils/api";
+
+type Params = ParsedUrlQuery & {
+  author: string;
+};
+
+export const getStaticPaths: GetStaticPaths<Params> = async () => {
+  const posts = getPosts();
+
+  return {
+    paths: posts.map((post) => ({
+      params: {
+        author: post.author,
+      },
+    })),
+    fallback: false,
+  };
+};
+
+export const getStaticProps: GetStaticProps = () => ({
+  props: {},
+});
 
 const Posts: NextPage = () => {
   const router = useRouter();
@@ -9,7 +33,7 @@ const Posts: NextPage = () => {
     const { author } = router.query;
 
     if (typeof author === "string") {
-      router.push("/[author]/", `/${author}/`).catch(() => {});
+      router.push(`/${author}/`).catch(() => {});
     }
   }, [router]);
 
