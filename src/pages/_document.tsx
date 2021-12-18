a7f9302eb3e9c00ed71e19f68889a97fd880088f
Yusuke4869 <67366917+Yusuke4869@users.noreply.github.com>
2021-12-18 13:24:35 +0900

    add: adsense script

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,10 +1,18 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
 
+const GOOGLE_ADSENSE_ID = process.env.NEXT_PUBLIC_GOOGLE_ADSENSE_ID ?? "";
+
 class MyDocument extends Document {
   render(): JSX.Element {
     return (
       <Html lang="ja">
-        <Head />
+        <Head>
+          <script
+            async
+            src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${GOOGLE_ADSENSE_ID}`}
+            crossOrigin="anonymous"
+          />
+        </Head>
         <body>
           <Main />
           <NextScript />
